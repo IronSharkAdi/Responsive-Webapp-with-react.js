a24233bf270df864a7c8e7c97730f83ebbe026e5
Adi <aditto2001@gmail.com>
2020-12-08 18:52:52 +0600

    final

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,20 @@
-import React  from 'react'
+import React, { useState }  from 'react'
+import axios from 'axios'
 
-
+//mypersonalmailserverisa
 
 const  Contact = () => {
+    const url = 'https://mypersonalmailserverisa.herokuapp.com/'
+    const [data, setData] = useState({ name : '', from : 'isservice', email : '' , msg : ''});
+    const sendmail = (e) =>{
+        e.preventDefault();
+        axios.post(url , data)
+        alert("Thanks for contacting , I will be responding soon")
+        setData({ ...data , email : '' })
+        setData({ ...data , name : ''})
+        setData({ ...data , msg : ''})
+    }
+
     return(
         <>
         <section id="contact" className="pt-5">
@@ -13,21 +25,21 @@ const  Contact = () => {
                         <div className="container contact_div">
                             <div className="row">
                                 <div className="col-md-6 col-10 mx-auto">
-                                    <form action="">
+                                    <form onSubmit={sendmail} >
                                         <div class="mb-3">
                                             <label for="exampleFormControlInput1" class="form-label">Name</label>
-                                            <input type="text" required class="form-control" id="exampleFormControlInput1" placeholder="Enter your name" />
+                                            <input value={data.name} onChange={(e) => setData({ ...data , name : e.target.value})} type="text" required class="form-control"  placeholder="Enter your name" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                                            <input type="email" required class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                                            <input value={data.email} onChange={(e) => setData({ ...data ,email : e.target.value})} type="email" required class="form-control"  placeholder="name@example.com" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-                                            <textarea class="form-control" required id="exampleFormControlTextarea1" rows="3"></textarea>
+                                            <textarea value={data.msg} onChange={(e) => setData({ ...data , msg : e.target.value})} class="form-control" required  rows="3"></textarea>
                                         </div>
                                         <div class="col-12">
-                                            <a class="btn btn-primary" href="/">Submit form</a>
+                                            <button type="submit" class="btn btn-primary" href="/">Submit form</button>
                                         </div>
                                     </form>
                                 </div>
